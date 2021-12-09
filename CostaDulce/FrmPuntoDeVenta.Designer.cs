905f3d27e80ee2656cefd094a0b543c371da000b
TKSOHLT <kimisami31@gmail.com>
2021-12-08 21:22:31 -0600

    Union completa

--- CostaDulce/FrmPuntoDeVenta.Designer.cs
+++ CostaDulce/FrmPuntoDeVenta.Designer.cs
@@ -1,5 +1,4 @@
-﻿
-namespace CostaDulce
+﻿namespace CostaDulce
 {
     partial class FrmPuntoDeVenta
     {
@@ -29,7 +28,305 @@ namespace CostaDulce
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtFecha = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtCantidad = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtProducto = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBuscarProductos = new System.Windows.Forms.Button();
+            this.txtIdProducto = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            //this.ventasDataSet = new CostaDulce.VentasDataSet();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTotalAPagar = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtPagaCon = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtCambio = new System.Windows.Forms.TextBox();
+            this.btnVender = new System.Windows.Forms.Button();
+            this.IdProducto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Producto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SubTotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Eliminar = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtCantidad)).BeginInit();
+            //((System.ComponentModel.ISupportInitialize)(this.ventasDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtFecha);
+            this.groupBox1.Location = new System.Drawing.Point(78, 67);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(231, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Información venta";
+            // 
+            // txtFecha
+            // 
+            this.txtFecha.Location = new System.Drawing.Point(10, 38);
+            this.txtFecha.Name = "txtFecha";
+            this.txtFecha.Size = new System.Drawing.Size(100, 20);
+            this.txtFecha.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtCantidad);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.txtStock);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.txtPrecio);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txtProducto);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.btnBuscarProductos);
+            this.groupBox2.Controls.Add(this.txtIdProducto);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(78, 193);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(957, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Información producto";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(543, 47);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(120, 20);
+            this.txtCantidad.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(540, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Cantidad:";
+            // 
+            // txtStock
+            // 
+            this.txtStock.Location = new System.Drawing.Point(418, 47);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(100, 20);
+            this.txtStock.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(415, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Stock:";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(294, 47);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 6;
+            this.txtPrecio.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPrecio_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(291, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Precio:";
+            // 
+            // txtProducto
+            // 
+            this.txtProducto.Location = new System.Drawing.Point(165, 47);
+            this.txtProducto.Name = "txtProducto";
+            this.txtProducto.Size = new System.Drawing.Size(100, 20);
+            this.txtProducto.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(162, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Producto:";
+            // 
+            // btnBuscarProductos
+            // 
+            this.btnBuscarProductos.Location = new System.Drawing.Point(116, 45);
+            this.btnBuscarProductos.Name = "btnBuscarProductos";
+            this.btnBuscarProductos.Size = new System.Drawing.Size(21, 23);
+            this.btnBuscarProductos.TabIndex = 2;
+            this.btnBuscarProductos.Text = "btnBuscarProducto";
+            this.btnBuscarProductos.UseVisualStyleBackColor = true;
+            this.btnBuscarProductos.Click += new System.EventHandler(this.btnBuscarProductos_Click);
+            // 
+            // txtIdProducto
+            // 
+            this.txtIdProducto.Location = new System.Drawing.Point(10, 47);
+            this.txtIdProducto.Name = "txtIdProducto";
+            this.txtIdProducto.Size = new System.Drawing.Size(100, 20);
+            this.txtIdProducto.TabIndex = 1;
+            this.txtIdProducto.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtIdProducto_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id Producto:";
+            // 
+            // ventasDataSet
+            // 
+            /*this.ventasDataSet.DataSetName = "VentasDataSet";
+            this.ventasDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;*/
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.IdProducto,
+            this.Producto,
+            this.Precio,
+            this.Cantidad,
+            this.SubTotal,
+            this.Eliminar});
+            this.dataGridView1.Location = new System.Drawing.Point(78, 299);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(957, 534);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellPainting += new System.Windows.Forms.DataGridViewCellPaintingEventHandler(this.dataGridView1_CellPainting);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(1042, 202);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(110, 91);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1044, 344);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Total a pagar:";
+            // 
+            // txtTotalAPagar
+            // 
+            this.txtTotalAPagar.HideSelection = false;
+            this.txtTotalAPagar.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.txtTotalAPagar.Location = new System.Drawing.Point(1047, 372);
+            this.txtTotalAPagar.Name = "txtTotalAPagar";
+            this.txtTotalAPagar.Size = new System.Drawing.Size(100, 20);
+            this.txtTotalAPagar.TabIndex = 5;
+            this.txtTotalAPagar.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1044, 414);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Paga con:";
+            // 
+            // txtPagaCon
+            // 
+            this.txtPagaCon.Location = new System.Drawing.Point(1047, 446);
+            this.txtPagaCon.Name = "txtPagaCon";
+            this.txtPagaCon.Size = new System.Drawing.Size(100, 20);
+            this.txtPagaCon.TabIndex = 7;
+            this.txtPagaCon.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtPagaCon_KeyDown);
+            this.txtPagaCon.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPagaCon_KeyPress);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(1044, 482);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Cambio:";
+            // 
+            // txtCambio
+            // 
+            this.txtCambio.Location = new System.Drawing.Point(1047, 511);
+            this.txtCambio.Name = "txtCambio";
+            this.txtCambio.Size = new System.Drawing.Size(100, 20);
+            this.txtCambio.TabIndex = 9;
+            // 
+            // btnVender
+            // 
+            this.btnVender.Location = new System.Drawing.Point(1047, 578);
+            this.btnVender.Name = "btnVender";
+            this.btnVender.Size = new System.Drawing.Size(106, 97);
+            this.btnVender.TabIndex = 10;
+            this.btnVender.Text = "Vender";
+            this.btnVender.UseVisualStyleBackColor = true;
+            this.btnVender.Click += new System.EventHandler(this.btnVender_Click);
+            // 
+            // IdProducto
+            // 
+            this.IdProducto.FillWeight = 50F;
+            this.IdProducto.HeaderText = "";
+            this.IdProducto.Name = "IdProducto";
+            this.IdProducto.Width = 50;
+            // 
+            // Producto
+            // 
+            this.Producto.HeaderText = "Producto";
+            this.Producto.Name = "Producto";
+            // 
+            // Precio
+            // 
+            this.Precio.HeaderText = "Precio";
+            this.Precio.Name = "Precio";
+            // 
+            // Cantidad
+            // 
+            this.Cantidad.HeaderText = "Cantidad";
+            this.Cantidad.Name = "Cantidad";
+            // 
+            // SubTotal
+            // 
+            this.SubTotal.HeaderText = "Sub Total";
+            this.SubTotal.Name = "SubTotal";
+            // 
+            // Eliminar
+            // 
+            this.Eliminar.FillWeight = 50F;
+            this.Eliminar.HeaderText = "";
+            this.Eliminar.Name = "Eliminar";
+            this.Eliminar.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Eliminar.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Eliminar.Width = 50;
             // 
             // FrmPuntoDeVenta
             // 
@@ -37,13 +334,65 @@ namespace CostaDulce
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1224, 741);
             this.ControlBox = false;
+            this.Controls.Add(this.btnVender);
+            this.Controls.Add(this.txtCambio);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtPagaCon);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtTotalAPagar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FrmPuntoDeVenta";
             this.Text = "FrmPuntoDeVenta";
+            this.Load += new System.EventHandler(this.FrmPuntoDeVenta_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtCantidad)).EndInit();
+            //((System.ComponentModel.ISupportInitialize)(this.ventasDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.NumericUpDown txtCantidad;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtStock;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtProducto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBuscarProductos;
+        private System.Windows.Forms.TextBox txtIdProducto;
+        private System.Windows.Forms.Label label1;
+        //private VentasDataSet ventasDataSet;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtTotalAPagar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtPagaCon;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtCambio;
+        private System.Windows.Forms.Button btnVender;
+        private System.Windows.Forms.TextBox txtFecha;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IdProducto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Producto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Precio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cantidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SubTotal;
+        private System.Windows.Forms.DataGridViewButtonColumn Eliminar;
     }
 }
--- CostaDulce/FrmPuntoDeVenta.cs
+++ CostaDulce/FrmPuntoDeVenta.cs
@@ -1,20 +1,269 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
+using System.Data.SqlClient;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Drawing;
+using CostaDulce.Properties;
 
 namespace CostaDulce
 {
     public partial class FrmPuntoDeVenta : Form
     {
+        SqlConnection connection = new SqlConnection("server=localhost;database=Ventas;integrated security=true");
+
+        //private Usuario usuario
         public FrmPuntoDeVenta()
         {
+            //(oUsuario=null)
+            //usuario = oUsuario();
             InitializeComponent();
+
         }
+
+        private void FrmPuntoDeVenta_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                connection.Open();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error en conexión");
+            }
+            //----------------------------------
+
+            txtFecha.Text = DateTime.Now.ToString("dd/MM/yyyy");
+            txtIdProducto.Text = "";
+
+            txtPagaCon.Text = "";
+            txtCambio.Text = "";
+            txtTotalAPagar.Text = "0";
+            txtCantidad.Text = "1";
+        }
+
+        private void btnBuscarProductos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtIdProducto_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (txtIdProducto.Text == "0")
+            {
+                MessageBox.Show("Debe seleccionar un producto", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else if (e.KeyData == Keys.Enter)
+            {
+
+                SqlCommand command = new SqlCommand(
+                    "select producto from ventasProductos where idProducto = " + txtIdProducto.Text + ";", connection);
+                SqlDataReader reader = command.ExecuteReader();
+                reader.Read();
+
+                txtProducto.Text = reader.GetString(0);
+
+                reader.Close();
+
+                command = new SqlCommand("select precioUnit from ventasProductos where idProducto = " + txtIdProducto.Text + ";", connection);
+                reader = command.ExecuteReader();
+                reader.Read();
+
+                txtPrecio.Text = reader.GetInt32(0).ToString();
+
+                reader.Close();
+
+                command = new SqlCommand("select stock from ventasProductos where idProducto = " + txtIdProducto.Text + ";", connection);
+                reader = command.ExecuteReader();
+                reader.Read();
+
+                txtStock.Text = reader.GetInt32(0).ToString();
+                reader.Close();
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            decimal precio = 0;
+            bool producto_existe = false;
+
+            if (int.Parse(txtIdProducto.Text) == 0)
+            {
+                MessageBox.Show("Debe seleccionar un producto", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            if (!decimal.TryParse(txtPrecio.Text, out precio))
+            {
+                MessageBox.Show("Precio - formato moneda incorrecto", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                txtPrecio.Select();
+                return;
+            }
+
+            if (Convert.ToInt32(txtStock.Text) < Convert.ToInt32(txtCantidad.Value.ToString()))
+            {
+                MessageBox.Show("La cantidad no puede ser mayor al stock", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            dataGridView1.Rows.Add(new object[]{
+            txtIdProducto.Text,
+            txtProducto.Text,
+            precio.ToString("0.00"),
+            txtCantidad.Value.ToString(),
+            (txtCantidad.Value * precio).ToString("0.00")
+            });
+
+            calcularTotal();
+            limpiarProducto();
+        }
+
+        private void calcularTotal()
+        {
+            decimal total = 0;
+
+            if (dataGridView1.Rows.Count > 0)
+            {
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                    total += Convert.ToDecimal(row.Cells["SubTotal"].Value.ToString());
+            }
+            txtTotalAPagar.Text = total.ToString("0.00");
+        }
+
+        private void limpiarProducto()
+        {
+            txtIdProducto.Text = "0";
+            txtProducto.Text = "";
+            txtPrecio.Text = "";
+            txtStock.Text = "";
+            txtCantidad.Text = "1";
+        }
+
+        private void dataGridView1_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+
+            if (e.RowIndex < 0)
+                return;
+
+            if (e.ColumnIndex == 5)
+            {
+                e.Paint(e.CellBounds, DataGridViewPaintParts.All);
+
+                var w = 50;
+                var h = 100;
+                var x = e.CellBounds.Left + (e.CellBounds.Width - w) / 2;
+                var y = e.CellBounds.Left + (e.CellBounds.Height - h) / 2;
+
+                //e.Graphics.DrawImage(Properties.Resources.borrar, new Rectangle(x, y, w, h));
+                e.Handled = true;
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.Columns[e.ColumnIndex].Name == "Eliminar")
+            {
+                int index = e.RowIndex;
+                if (index >= 0)
+                {
+                    dataGridView1.Rows.RemoveAt(index);
+                    calcularTotal();
+                }
+            }
+        }
+
+        private void txtPrecio_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else
+                if (txtPrecio.Text.Trim().Length == 0 && e.KeyChar.ToString() == ".")
+            {
+                e.Handled = true;
+            }
+            else
+                if (char.IsControl(e.KeyChar) || e.KeyChar.ToString() == ".")
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtPagaCon_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else
+                if (txtPrecio.Text.Trim().Length == 0 && e.KeyChar.ToString() == ".")
+            {
+                e.Handled = true;
+            }
+            else
+                if (char.IsControl(e.KeyChar) || e.KeyChar.ToString() == ".")
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void calcularCambio()
+        {
+            if (txtTotalAPagar.Text.Trim() == "")
+            {
+                MessageBox.Show("No existen productos en la venta", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+            decimal pagaCon;
+            decimal total = Convert.ToDecimal(txtTotalAPagar.Text);
+
+            if (txtPagaCon.Text.Trim() == "")
+            {
+                txtPagaCon.Text = "0";
+            }
+
+            if (decimal.TryParse(txtPagaCon.Text.Trim(), out pagaCon))
+            {
+                if (pagaCon < total)
+                {
+                    txtCambio.Text = "0.00";
+                }
+                else
+                {
+                    decimal cambio = pagaCon - total;
+                    txtCambio.Text = cambio.ToString("0.00");
+                }
+            }
+        }
+
+        private void txtPagaCon_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                calcularCambio();
+            }
+        }
+
+        private void btnVender_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i <= dataGridView1.RowCount; i++)
+                //if (index >= 0)
+                dataGridView1.Rows.RemoveAt(i);
+        }
+
     }
 }
